16261ef89484beaacf589bc2b0c3a00474601e35
Ian Sowinski <sowinj@gmail.com>
2017-02-15 22:28:18 +0100

    added public functions for writing in character mode

--- printer.rb
+++ printer.rb
@@ -10,8 +10,70 @@ class Printer
   def write (args)
     @printer.write(args)
   end
+  # character mode settings
+  def font_b (string)
+    set_characters("1")
+    @printer.write(string)
+    clear
+  end
+  def invert (string)
+    set_characters("2")
+    @printer.write(string)
+    clear
+  end
+  def upside_down (string)
+    set_characters("4")
+    @printer.write(string)
+    clear
+  end
+  def emphasize (string)d
+    set_characters("8")
+    @printer.write(string)
+    clear
+  end
+  def double_height (string)
+    set_characters("16")
+    @printer.write(string)
+    clear
+  end
+  def double_width (string)
+    set_characters("32")
+    @printer.write(string)
+    clear
+  end
+  def deleteline (string)
+    set_characters("64")
+    @printer.write(string)
+    clear
+  end
   private
+  # function to clear character mode settings
   def clear
+    @printer.write(@@ESC)
+    @printer.write("!")
+    @printer.write("0")
+  end
+  # function to set character mode
+  def set_characters (mode)
+#    case mode
+#    when "font_b"
+#      mode = "1"
+#    when "invert"
+#      mode = "2"
+#    when "upside_down"
+#      mode = "4"
+#    when "emphasized"
+#      mode = "8"
+#    when "double_height"
+#      mode = "16"
+#    when "double_width"
+#      mode = "32"
+#    when "deleteline"
+#      mode = "64"
+#    end
+    @printer.write(@@ESC)
+    @printer.write("!")
+    @printer.write(mode)
   end
 #  def no_name_function (heatTime=80, heatInterval=2, heatingDots=7)
 #  don't know what to do with this. It's for setting up bitmapsk  
