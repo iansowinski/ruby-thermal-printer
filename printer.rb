61bce5f3099a55fa1d9d2e8f228d91f0f5648166
Ian Sowinski <sowinj@gmail.com>
2017-02-15 21:00:19 +0100

    made indents how it shoul be

--- printer.rb
+++ printer.rb
@@ -1,30 +1,30 @@
 require 'rubyserial'
 class Printer
-    @@ESC = 27.chr
-    def initialize (serial_port='/dev/ttyAMA0', baudrate=19200)
-          @printer = Serial.new(serial_port, baudrate)
-          # for initializing printer
-          @printer.write(@@ESC)
-          @printer.write(64.chr)
-    end
-    def write (args)
-      @printer.write(args)
-    end
-    private
-    def clear
-    end
-    # def no_name_function (heatTime=80, heatInterval=2, heatingDots=7)
-    # don't know what to do with this. It's for setting up bitmapsk  
-    #   @printer.write(@@ESC)
-    #   @printer.write(55.chr)
-    #   @printer.write(heatingDots.chr)
-    #   @printer.write(heatTime.chr) 
-    #   @printer.write(heatInterval.chr)
-    #   printDensity = 15
-    #   printBreakTime = 15 
-    #   @printer.write(18.chr)
-    #   @printer.write(35.chr)
-    #   @printer.write(((printDensity << 4) | printBreakTime).chr)
+  @@ESC = 27.chr
+  def initialize (serial_port='/dev/ttyAMA0', baudrate=19200)
+    @printer = Serial.new(serial_port, baudrate)
+    # for initializing printer
+    @printer.write(@@ESC)
+    @printer.write(64.chr)
+  end
+  def write (args)
+    @printer.write(args)
+  end
+  private
+  def clear
+  end
+#  def no_name_function (heatTime=80, heatInterval=2, heatingDots=7)
+#  don't know what to do with this. It's for setting up bitmapsk  
+#    @printer.write(@@ESC)
+#    @printer.write(55.chr)
+#    @printer.write(heatingDots.chr)
+#    @printer.write(heatTime.chr) 
+#    @printer.write(heatInterval.chr)
+#    printDensity = 15
+#    printBreakTime = 15 
+#    @printer.write(18.chr)
+#    @printer.write(35.chr)
+#    @printer.write(((printDensity << 4) | printBreakTime).chr)
 end
 
 printer = Printer.new
