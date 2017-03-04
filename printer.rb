063e9bee9f1fa6860429935bddbec4899514d3e0
Ian Sowiński <sowinj@gmail.com>
2017-03-04 16:19:14 +0100

    modes working

--- printer.rb
+++ printer.rb
@@ -5,6 +5,7 @@ class Printer
   def initialize (serial_port='/dev/ttyAMA0', baudrate=19200, heatTime=80, heatInterval=2, heatingDots=7)
     @printer = Serial.new(serial_port, baudrate)
     # for initializing printer
+    puts "printer initialized"
     @printer.write(@@ESC)
     @printer.write(64.chr)
     @printer.write(@@ESC)
@@ -17,87 +18,69 @@ class Printer
     @printer.write(18.chr)
     @printer.write(35.chr)
     @printer.write(((printDensity << 4) | printBreakTime).chr)
+    puts "settings set"
   end
   def write (args)
     @printer.write(args)
     linefeed
+    puts "writed"
   end
   # character mode settings
   def font_b (string)
-    set_characters(1.chr)
+    set_characters(0)
     @printer.write(string)
     clear
     linefeed
+  end
+  def clockwise_rotation(string)
+    @printer.write("#{27.chr}V1")
+    write(string)
+    @printer.write("#{27.chr}V0")
   end
   def invert (string)
-    set_characters(2.chr)
-    @printer.write(string)
-    clear
-    linefeed
+    @printer.write("#{29.chr}B1")
+    write(string)
+    @printer.write("#{29.chr}B0")
   end
   def upside_down (string)
-    set_characters(4.chr)
-    @printer.write(string)
-    clear
-    linefeed
+    @printer.write("#{27.chr}{1")
+    write(string)
+    @printer.write("#{27.chr}{0")
   end
   def emphasize (string)
-    set_characters(8.chr)
-    @printer.write(string)
-    clear
-    linefeed
+    @printer.write("#{27.chr}E1")
+    write(string)
+    @printer.write("#{27.chr}E0")
   end
   def double_height (string)
-    set_characters(16.chr)
+    set_characters(4)
     @printer.write(string)
     clear
     linefeed
   end
   def double_width (string)
-    set_characters(32.chr)
+    set_characters(5)
     @printer.write(string)
     clear
     linefeed
   end
   def deleteline (string)
-    set_characters(64.chr)
+    set_characters(6)
     @printer.write(string)
     clear
     linefeed
   end
-  private
-  # function to clear character mode settings
-  def clear
-    @printer.write(@@ESC)
-    @printer.write(33.chr)
-    @printer.write(0.chr)
-  end
   def linefeed
     @printer.write(10.chr)
   end
-  # function to set character mode
-  def set_characters (mode)
-#    case mode
-#    when "font_b"
-#      mode = "1"
-#    when "invert"
-#      mode = "2"
-#    when "upside_down"
-#      mode = "4"
-#    when "emphasized"
-#      mode = "8"
-#    when "double_height"
-#      mode = "16"
-#    when "double_width"
-#      mode = "32"
-#    when "deleteline"
-#      mode = "64"
-#    end
-    @printer.write(@@ESC)
-    @printer.write(33.chr)
-    @printer.write(mode)
-  end
-#  def no_name_function (
-#  don't know what to do with this. It's for setting up bitmapsk  
-
 end
+
+printer = Printer.new()
+#printer.write("1234567")
+#printer.write(1234)
+#printer.font_b("abc")
+printer.invert("abecadle")
+printer.emphasize("abecadle")
+printer.upside_down("abecadle")
+printer.clockwise_rotation("abecadle")
+printer.invert("abecadle")
